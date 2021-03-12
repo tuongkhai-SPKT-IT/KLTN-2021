09eebe543a724ad5e0e4163639f67a572c6362dc
LAPTOP-Q0JDMI0M\DoTuongKhai <dotuongkhai191999@gmail.com>
2021-03-12 15:39:57 +0700

    modal comment

--- src/components/ContentStatus/ViewLCS.js
+++ src/components/ContentStatus/ViewLCS.js
@@ -1,12 +1,13 @@
 import MainContent from './MainContent';
-import React, {Component, useRef, useState, useEffect} from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import {
   View,
   TextInput,
   ScrollView,
   Text,
   Image,
-  TouchableOpacity,
+  Pressable,
+  BackHandler,
 } from 'react-native';
 import AntDesignIcon from 'react-native-vector-icons/AntDesign';
 import FontAwesome5nIcon from 'react-native-vector-icons/FontAwesome5';
@@ -14,16 +15,15 @@ import FontAwesomenIcon from 'react-native-vector-icons/FontAwesome';
 import API from '../API/API';
 import * as styles from './Styles';
 import {Button} from 'react-native-elements';
-import Modal from 'react-native-modalbox';
+import SwipeDownModal from 'react-native-swipe-down';
 import * as keys from '../Constants';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {Pressable} from 'react-native';
 export default function ViewLCS(props) {
   const [listLike, setListLike] = useState(props.likeList);
   const [likeNumber, setLikeNumber] = useState(props.likeNumber);
-  const userToken = useRef('');
+  const scrollRef = useRef(null);
   const userInfo = useRef({});
-  const modalRef = useRef(null);
+  const [visible, setVisible] = useState(false);
   const [liked, setLiked] = useState(props.liked);
   const [loadding, setLoadding] = useState(false);
   const [inputCmt, setInputCmt] = useState('');
@@ -32,10 +32,10 @@ export default function ViewLCS(props) {
     listComment.slice(listComment.length - 2),
   );
   const textComment = useRef(null);
+
   useEffect(() => {
     AsyncStorage.getItem(keys.User_Token).then((val) => {
       if (val) {
-        userToken.current = val;
         userInfo.current = {...userInfo.current, user_token: val};
       }
     });
@@ -54,20 +54,20 @@ export default function ViewLCS(props) {
         userInfo.current = {...userInfo.current, user_ProLink: val};
       }
     });
-    console.log(listComment);
+    // console.log(listComment);
   }, []);
-
   const Like = async () => {
     // console.log(userToken);
     setLoadding(true);
     if (!liked) {
+      console.log('like nha');
       const route = 'status/update-status';
       const param = {
         status_id: props.index,
         like: parseInt(likeNumber) + 1,
       };
       const header = {
-        Authorization: 'bearer' + userToken.current,
+        Authorization: 'bearer' + userInfo.current.user_token,
       };
       const api = new API();
       // console.log(param, route, header);
@@ -78,7 +78,7 @@ export default function ViewLCS(props) {
             if (res.data.error_code !== 0) {
               window.alert(res.data.message);
             } else {
-              console.log(res.data.data);
+              // console.log(res.data.data);
               setListLike(res.data.data);
               setLiked(true);
               setLoadding(false);
@@ -97,7 +97,7 @@ export default function ViewLCS(props) {
         is_unlike: 1,
       };
       const header = {
-        Authorization: 'bearer' + userToken.current,
+        Authorization: 'bearer' + userInfo.current.user_token,
       };
       const api = new API();
       // console.log(param, route, header);
@@ -109,7 +109,7 @@ export default function ViewLCS(props) {
               window.alert(res.data.message);
             } else {
               setListLike(res.data.data);
-              console.log(res.data.data);
+              // console.log(res.data.data);
               setLiked(false);
               setLoadding(false);
               setLikeNumber(likeNumber - 1);
@@ -121,12 +121,49 @@ export default function ViewLCS(props) {
       }, 2 * 1000);
     }
   };
+  const inputComment = () => {
+    return (
+      // <ScrollView keyboardShouldPersistTaps="handled" style={{flex: 1}}>
+      <View style={styles.stylesViewLCS.inputcmtContainer}>
+        <TextInput
+          ref={textComment}
+          onChangeText={(e) => setInputCmt(e)}
+          placeholder="Viết 1 bình luận"
+          style={[styles.stylesViewLCS.textCmt]}
+          value={inputCmt}
+        />
 
+        <Pressable style={styles.stylesViewLCS.btnComment}>
+          <FontAwesomenIcon
+            onPress={() => {
+              if (inputCmt.trim() !== '') {
+                listComment.push({
+                  comment: inputCmt,
+                  linkProfile: userInfo.current.user_ProLink,
+                  user_avatar: userInfo.current.user_avatar,
+                  user_name: userInfo.current.user_name,
+                });
+                setSmallCmt(listComment.slice(listComment.length - 2));
+                if (textComment.current) textComment.current.clear();
+                setInputCmt('');
+              }
+            }}
+            name="send"
+            style={{width: 26}}
+            size={25}
+            color="rgba(0,0,0,.8)"
+          />
+        </Pressable>
+      </View>
+      // </ScrollView>
+    );
+  };
   const SingleComment = (comment, i) => {
     return (
       <Pressable
         onPress={() => {
-          modalRef.current.open();
+          setVisible(true);
+          // setCmtVisible(true);
         }}
         key={i}
         style={styles.stylesViewLCS.SingleCommentContainer}>
@@ -145,12 +182,72 @@ export default function ViewLCS(props) {
       </Pressable>
     );
   };
+  const whoLike = (list) => {
+    var i = 0;
+    var listWho = '';
+    if (list.length <= 2)
+      for (i = 0; i < list.length; i++) {
+        if (i + 1 < list.length)
+          listWho = list[i].user_name + ', ' + list[i + 1].user_name;
+      }
+    return listWho;
+  };
+
+  const toggleOverlay = () => {
+    setVisible(false);
+  };
   return (
     <>
+      <SwipeDownModal
+        modalVisible={visible}
+        //if you don't pass HeaderContent you should pass marginTop in view of ContentModel to Make modal swipeable
+        ContentModal={
+          <View style={styles.stylesViewLCS.containerContent}>
+            {listLike.length > 0 && (
+              <Pressable
+                onPress={() => {
+                  console.log(listLike);
+                }}
+                style={[
+                  {borderTopLeftRadius: 16, borderTopRightRadius: 16},
+                  styles.stylesViewLCS.iconLike,
+                  styles.stylesViewLCS.extendLike,
+                ]}>
+                <View style={styles.stylesViewLCS.viewIcon}>
+                  <AntDesignIcon name="like1" size={15} color="white" />
+                </View>
+                <Text style={styles.stylesViewLCS.likeNumber}>
+                  {whoLike(listLike)}
+                </Text>
+              </Pressable>
+            )}
+            <View style={{flex: 10}}>
+              <ScrollView
+                ref={scrollRef}
+                overScrollMode="always"
+                contentContainerStyle={{justifyContent: 'center'}}
+                // showsHorizontalScrollIndicator={false}
+                snapToInterval={450}
+                decelerationRate="fast"
+                onScroll={(event) =>
+                  console.log(event.nativeEvent.contentOffset)
+                }
+                style={{marginTop: 10}}>
+                {listComment.map(SingleComment)}
+              </ScrollView>
+            </View>
+            {inputComment()}
+          </View>
+        }
+        ContentModalStyle={styles.stylesViewLCS.Modal}
+        onClose={() => {
+          setVisible(false);
+        }}
+      />
       {listLike.length > 0 && (
         <Pressable
           onPress={() => {
-            modalRef.current.open();
+            // setCmtVisible(true);
           }}
           style={styles.stylesViewLCS.iconLike}>
           <View style={styles.stylesViewLCS.viewIcon}>
@@ -196,11 +293,10 @@ export default function ViewLCS(props) {
             />
           }
           loadingProps={{animating: true}}
-          loadingStyle={{}}
           title="Bình luận"
           onPress={() => {
-            // textComment.current.focus();
-            AsyncStorage.clear();
+            if (textComment.current) textComment.current.focus();
+            // AsyncStorage.clear();
           }}
           titleStyle={{
             marginHorizontal: 5,
@@ -211,70 +307,7 @@ export default function ViewLCS(props) {
         />
       </View>
       <View style={{marginVertical: 10}}>{smallCmt.map(SingleComment)}</View>
-      <Modal
-        position="center"
-        coverScreen
-        backButtonClose
-        // swipeToClose={false}
-        ref={modalRef}>
-        {listLike.length > 0 && (
-          <Pressable
-            onPress={() => {
-              modalRef.current.open();
-            }}
-            style={[
-              styles.stylesViewLCS.iconLike,
-              styles.stylesViewLCS.extendLike,
-            ]}>
-            <View style={styles.stylesViewLCS.viewIcon}>
-              <AntDesignIcon name="like1" size={15} color="white" />
-            </View>
-            <Text style={styles.stylesViewLCS.likeNumber}>
-              {listLike.length}
-            </Text>
-          </Pressable>
-        )}
-        <View style={{flex: 9}}>
-          <ScrollView
-            showsHorizontalScrollIndicator={false}
-            snapToInterval={450}
-            decelerationRate="fast"
-            style={{marginTop: 20}}>
-            {listComment.map(SingleComment)}
-          </ScrollView>
-        </View>
-      </Modal>
-      <ScrollView keyboardShouldPersistTaps="handled" style={{flex: 1}}>
-        <TextInput
-          ref={textComment}
-          onChangeText={(e) => setInputCmt(e)}
-          placeholder="Viết 1 bình luận"
-          style={styles.stylesViewLCS.textCmt}
-        />
-
-        <TouchableOpacity
-          style={{
-            position: 'absolute',
-            top: 10,
-            height: '100%',
-            right: 15,
-          }}
-          onPress={() => {
-            if (inputCmt.trim() !== '') {
-              listComment.push({
-                comment: inputCmt,
-                linkProfile: userInfo.current.user_ProLink,
-                user_avatar: userInfo.current.user_avatar,
-                user_name: userInfo.current.user_name,
-              });
-              setSmallCmt(listComment.slice(listComment.length - 2));
-              textComment.current.clear();
-              setInputCmt('');
-            }
-          }}>
-          <FontAwesomenIcon name="send" size={25} color="rgba(0,0,0,.8)" />
-        </TouchableOpacity>
-      </ScrollView>
+      {inputComment()}
     </>
   );
 }
